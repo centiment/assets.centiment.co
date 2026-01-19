978d671678062a14d2ee6050a13bc3dfcbc58f1f
Filip Rurak <filip.rurak@movecloser.pl>
2026-01-19 10:32:52 +0100

    feat/webflow/pricing-page

---
+++ scripts/webflow/pricing-page/index.js
@@ -0,0 +1,143 @@
+/**
+ * Centiment
+ *
+ * @author    Filip Rurak
+ * @copyright Centiment LLC.
+ * @link      https://centiment.co
+ */
+
+const Enums = {
+    RecurringInterval: {
+        Monthly: "monthly",
+        Yearly: "yearly",
+    },
+};
+
+const Prices = {
+    Professional: {
+        [Enums.RecurringInterval.Monthly]: {
+            "4000": 450,
+            "6000": 675,
+            "8000": 900,
+            "10000": 1125,
+            "12000": 1350,
+            "14000": 1575,
+            "16000": 1800,
+        },
+        [Enums.RecurringInterval.Yearly]: {
+            "4000": 400,
+            "6000": 600,
+            "8000": 800,
+            "10000": 1000,
+            "12000": 1200,
+            "14000": 1400,
+            "16000": 1600,
+        },
+    },
+};
+
+let State = {
+    recurringInterval: Enums.RecurringInterval.Monthly,
+    interactions: 0,
+    sliderValue: 0,
+    sliderMax: 7,
+};
+
+const Helper = {
+    formatPrice: (number) => number.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    formatReadable: (number) => (Number(number) || 0).toLocaleString("en-US"),
+    getProfessionalPlanInteractionsList: () => Object.keys(Prices.Professional[State.recurringInterval]).map(key => parseInt(key)).sort((a, b) => a - b),
+};
+
+/**
+ * Run interface updates
+ */
+const runInterfaceUpdates = () => {
+    const _runProfessionalPlanCardUpdates = () => {
+        const priceValueElement = document.getElementById("professional-price-value");
+        const priceIntervalElement = document.getElementById("professional-price-interval");
+
+        // 1. Update price value — e.g. "$1,200"
+        if (Prices.Professional[State.recurringInterval][State.interactions]) {
+            priceValueElement.innerText = Helper.formatPrice(Prices.Professional[State.recurringInterval][State.interactions]);
+        }
+
+        // 1. Update recurring interval — e.g. "billing monthly"
+        priceIntervalElement.innerText = State.recurringInterval;
+
+        const cardElement = document.getElementById("professional-card");
+        const cardInteractionsElement = document.getElementById("professional-card-interactions");
+        const cardButtonLabelElement = document.getElementById("professional-card-button-label");
+
+        // 1. Update interactions bullet point — e.g. "12,000 interactions"
+        // 2. Update button label
+        // 3. Toggle disabled class on plan card
+        if (Helper.getProfessionalPlanInteractionsList().includes(State.interactions)) {
+            cardInteractionsElement.innerText = `${Helper.formatReadable(State.interactions)} interactions`;
+            cardButtonLabelElement.innerText = "Get Professional";
+            cardElement.classList.remove("--disabled");
+        } else {
+            cardButtonLabelElement.innerText = "Not applicable";
+            cardElement.classList.add("--disabled");
+        }
+    };
+
+    const _runSliderUpdates = () => {
+        const sliderPatternElement = document.getElementById("interactions-slider-pattern");
+
+        const percentage = Math.min(100, (State.sliderValue / State.sliderMax) * 100);
+        sliderPatternElement.style.width = `${percentage}%`;
+    }
+
+    _runProfessionalPlanCardUpdates();
+    _runSliderUpdates();
+};
+
+/**
+ * Watch for input switch updates
+ */
+const watchRecurringIntervalChanges = () => {
+    const switchInput = document.getElementById("recurring-interval-switch");
+
+    switchInput?.addEventListener("click", () => {
+        State.recurringInterval = State.recurringInterval === Enums.RecurringInterval.Monthly
+            ? Enums.RecurringInterval.Yearly
+            : Enums.RecurringInterval.Monthly;
+
+        switchInput.classList.remove(`--${Enums.RecurringInterval.Yearly}`, `--${Enums.RecurringInterval.Monthly}`);
+        switchInput.classList.add(`--${State.recurringInterval}`);
+
+        runInterfaceUpdates();
+    });
+};
+
+/**
+ * Watch for range slider updates
+ */
+const watchInteractionsChanges = () => {
+    const rangeInput = document.getElementById("interactions-slider");
+
+    rangeInput?.addEventListener("input", (e) => {
+        State.sliderValue = parseInt(e.target.value);
+        State.interactions = Helper.getProfessionalPlanInteractionsList()[State.sliderValue];
+
+        runInterfaceUpdates();
+    });
+};
+
+/**
+ * Initialize interactions state
+ */
+const setLowestInteractions = () => {
+    State.interactions = Helper.getProfessionalPlanInteractionsList()[0];
+};
+
+const initializePricingPage = () => {
+    setLowestInteractions();
+    runInterfaceUpdates();
+
+    watchRecurringIntervalChanges();
+    watchInteractionsChanges();
+};
+
+document.addEventListener("DOMContentLoaded", initializePricingPage);
