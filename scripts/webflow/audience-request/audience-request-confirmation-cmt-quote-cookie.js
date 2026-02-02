beed74f38923f7f5618fd327ae8746a3db0a66cc
Filip Rurak <filip.rurak@movecloser.pl>
2026-02-02 14:42:51 +0100

    updated cookie name

--- scripts/webflow/audience-request/audience-request-confirmation-cmt-quote-cookie.js
+++ scripts/webflow/audience-request/audience-request-confirmation-cmt-quote-cookie.js
@@ -12,6 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const expires = new Date(Date.now() + (2 * 60 * 60 * 1000)).toUTCString();
 
-    document.cookie = `CMT-QUOTE=${confirmEmail ?? "empty"}; expires=${new Date(0).toUTCString()}; path=/; domain=.centiment.co; SameSite=Lax`;
-    document.cookie = `CMT-QUOTE=${confirmEmail ?? "empty"}; expires=${expires}; path=/; domain=.centiment.co; SameSite=Lax`;
+    document.cookie = `CMT-QUOTE-SUBMIT-EMAIL=${confirmEmail ?? "empty"}; expires=${expires}; path=/; domain=.centiment.co; SameSite=Lax`;
 });
